c5e7b7640ab6828274d0d8584c8aebd248e43acf
fabriziomarengo <fabriziomarengo97@gmail.com>
2024-01-16 23:30:27 -0300

    16-1

---
+++ template-ecommerce-main/src/firebaseConfig.js
@@ -0,0 +1,34 @@
+
+import { initializeApp } from "firebase/app";
+
+import{signInWithEmailAndPassword,getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCn-9xc4jqAuqw0thQm-QHas5PydLberKo",
+  authDomain: "workshop-test-b4bf8.firebaseapp.com",
+  projectId: "workshop-test-b4bf8",
+  storageBucket: "workshop-test-b4bf8.appspot.com",
+  messagingSenderId: "942780801498",
+  appId: "1:942780801498:web:48fe322b0d134642e9e0e3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth  = getAuth(app)
+//SERVICIOS DE FIREBEASE
+
+//auth
+
+//login
+const login  
+
+//registro
+//logout
+//olvide pass
+
+const auth  = getAuth(app)
+
+
+//database firestore
+//storage
+
